4a59ddb0484f9da5a3ffa105d84bf402e006c573
Jennifer Cooper <31331976+jrcooper91@users.noreply.github.com>
2019-04-21 16:45:55 -0500

    Update make_table.py

--- make_table.py
+++ make_table.py
@@ -1,6 +1,7 @@
 import glob
 import time
 import os
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,17 +17,14 @@ for file in os.listdir(directory):
      filename = os.fsdecode(file)
      if filename.endswith("full.fits"): 
          fit_hdu = pyfits.open(filename) 
-         id = fit_hdu[0].header['ID']
+         ids = fit_hdu[0].header['ID']
          ra = fit_hdu[0].header['RA']
          dec = fit_hdu[0].header['DEC']
          z = fit_hdu[0].header['REDSHIFT']
          Ha = fit_hdu[2].header['FLUX_004']
          Ha_err = fit_hdu[2].header['ERR_004']
-         print(id,ra,dec,z,Ha,Ha_err)
-         my_list=id,ra,dec,z,Ha,Ha_err
-         with open('j130132m1142txt', 'a+') as f:
-             for item in my_list:
-                 f.write("%s\n" % item)
+         sys.stdout = open("t.txt", "a")
+         print(ids,ra,dec,z,Ha,Ha_err) 
          continue
      else:
          continue
