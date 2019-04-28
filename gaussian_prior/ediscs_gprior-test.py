ef7d19e7bfcdf4f028dd0e9813c751d912e41ce6
Jennifer Cooper <31331976+jrcooper91@users.noreply.github.com>
2019-04-28 15:47:49 -0500

    Add files via upload

--- gaussian_prior/ediscs_gprior-test.py
+++ gaussian_prior/ediscs_gprior-test.py
@@ -84,40 +84,66 @@ def c1301(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     y1 = [1.3,1.3]
     y2 = [1.3,2]
     y3 = [2,5]
+
+    f = open('/data2/jrcooper/notebooks/reduction/EDisCS/Ha_catalogs/gaussian_prior/1301_LDP.txt', 'r')
+    lines = f.readlines()[1:]
+    f.close()
+    
+    #create arrays 
+    LDP_UV    = [] 
+    LDP_VJ   = []
+    
+    
+    #pull array column 
+    for line in lines: 
+        a = line.split()
+        LDP_UV.append(float(a[3]))
+        LDP_VJ.append(float(a[2]))
+        
+        
+    LDP_UV = np.array(LDP_UV)
+    LDP_VJ = np.array(LDP_VJ)
+
     
     plt.subplot(3,2,1)
-    plt.scatter(crf_VJ[np.where(np.logical_and(cz>0.4628,cz<0.5028))],crf_UV[np.where(np.logical_and(cz>0.4628,cz<0.5028))],c=cSFR[np.where(np.logical_and(cz>0.4628,cz<0.5028))])
+    plt.hist2d(LDP_VJ-0.155, LDP_UV-0.306, bins=(150, 150), cmap=plt.cm.Greys)
+    plt.scatter(crf_VJ[np.where(np.logical_and(cz>0.4628,cz<0.5028))]-0.155,crf_UV[np.where(np.logical_and(cz>0.4628,cz<0.5028))]-0.306,c=cSFR[np.where(np.logical_and(cz>0.4628,cz<0.5028))],s=100)
     cbar = plt.colorbar()
     cbar.set_label("log(SFR)")
+    
+    #plt.colorbar()
+
     plt.xlabel('V-J')
     plt.ylabel('U-V')
     plt.title('Cluster core UVJ z+/- 0.02')
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
     
     plt.subplot(3,2,2)
+    plt.hist2d(LDP_VJ-0.155, LDP_UV-0.306, bins=(150, 150), cmap=plt.cm.Greys)
     #plt.scatter(rf_VJ[np.where(np.logical_and(z<0.4628,z>0.5028))],rf_UV[np.where(np.logical_and(z<0.4628,z>0.5028))],c=SFR[np.where(np.logical_and(z<0.4628,z>0.5028))])
-    plt.scatter(crf_VJ[np.where(cz<0.4628)],crf_UV[np.where(cz<0.4628)],c=cSFR[np.where(cz<0.4628)])
-    plt.scatter(crf_VJ[np.where(cz>0.5028)],crf_UV[np.where(cz>0.5028)],c=cSFR[np.where(cz>0.5028)])
-    plt.scatter(grf_VJ[np.where(gz<0.4628)],grf_UV[np.where(gz<0.4628)],c=gSFR[np.where(gz<0.4628)])
-    plt.scatter(grf_VJ[np.where(gz>0.5028)],grf_UV[np.where(gz>0.5028)],c=gSFR[np.where(gz>0.5028)])
+    plt.scatter(crf_VJ[np.where(cz<0.4628)]-0.155,crf_UV[np.where(cz<0.4628)]-0.306,c=cSFR[np.where(cz<0.4628)],s=100)
+    plt.scatter(crf_VJ[np.where(cz>0.5028)]-0.155,crf_UV[np.where(cz>0.5028)]-0.306,c=cSFR[np.where(cz>0.5028)],s=100)
+    plt.scatter(grf_VJ[np.where(gz<0.4628)]-0.155,grf_UV[np.where(gz<0.4628)]-0.306,c=gSFR[np.where(gz<0.4628)],s=100)
+    plt.scatter(grf_VJ[np.where(gz>0.5028)]-0.155,grf_UV[np.where(gz>0.5028)]-0.306,c=gSFR[np.where(gz>0.5028)],s=100)
     cbar = plt.colorbar()
     cbar.set_label("log(SFR)")
-    cbar.set_clim(-0.2,0.8)
+    cbar.set_clim(-0.5,0.5)
     plt.xlabel('V-J')
     plt.ylabel('U-V')
     plt.title('Field UVJ')
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
 
     plt.subplot(3,2,3)
-    plt.scatter(grf_VJ[np.where(np.logical_and(gz>0.4628,gz<0.5028))],grf_UV[np.where(np.logical_and(gz>0.4628,gz<0.5028))],c=gSFR[np.where(np.logical_and(gz>0.4628,gz<0.5028))])
+    plt.hist2d(LDP_VJ-0.155, LDP_UV-0.306, bins=(150, 150), cmap=plt.cm.Greys)
+    plt.scatter(grf_VJ[np.where(np.logical_and(gz>0.4628,gz<0.5028))]-0.155,grf_UV[np.where(np.logical_and(gz>0.4628,gz<0.5028))]-0.306,c=gSFR[np.where(np.logical_and(gz>0.4628,gz<0.5028))],s=100)
     cbar = plt.colorbar()
     cbar.set_label("log(SFR)")
     plt.xlabel('V-J')
@@ -126,17 +152,17 @@ def c1301(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
    
     
     plt.subplot(3,2,4)
-    plt.scatter(crf_m[np.where((cz>0.4628)&(cz<0.5028))],cSFR[np.where((cz>0.4628)&(cz<0.5028))],color='blue',alpha=0.7,marker = 'o')
-    plt.scatter(grf_m[np.where((gz>0.4628)&(gz<0.5028))],gSFR[np.where((gz>0.4628)&(gz<0.5028))],color='blue',alpha=0.5,marker = 'x')
-    plt.scatter(crf_m[np.where(cz<0.4628)],cSFR[np.where(cz<0.4628)],c='red',alpha=0.5)
-    plt.scatter(crf_m[np.where(cz>0.5028)],cSFR[np.where(cz>0.5028)],c='red',alpha=0.5)
-    plt.scatter(grf_m[np.where(gz<0.4628)],gSFR[np.where(gz<0.4628)],c='red',alpha=0.5)
-    plt.scatter(grf_m[np.where(gz>0.5028)],gSFR[np.where(gz>0.5028)],c='red',alpha=0.5) 
+    plt.scatter(crf_m[np.where((cz>0.4628)&(cz<0.5028))],cSFR[np.where((cz>0.4628)&(cz<0.5028))],color='blue',alpha=0.7,marker = 'o',s=100)
+    plt.scatter(grf_m[np.where((gz>0.4628)&(gz<0.5028))],gSFR[np.where((gz>0.4628)&(gz<0.5028))],color='blue',alpha=0.5,marker = 'x',s=100)
+    plt.scatter(crf_m[np.where(cz<0.4628)],cSFR[np.where(cz<0.4628)],c='red',alpha=0.5,s=100)
+    plt.scatter(crf_m[np.where(cz>0.5028)],cSFR[np.where(cz>0.5028)],c='red',alpha=0.5,s=100)
+    plt.scatter(grf_m[np.where(gz<0.4628)],gSFR[np.where(gz<0.4628)],c='red',alpha=0.5,s=100)
+    plt.scatter(grf_m[np.where(gz>0.5028)],gSFR[np.where(gz>0.5028)],c='red',alpha=0.5,s=100) 
     plt.xlim(6.8,11)
     #plt.ylim(-1,3)
     plt.xlabel('logM')
@@ -250,8 +276,29 @@ def c1138(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     y2 = [1.3,2]
     y3 = [2,5]
 
+    f = open('/data2/jrcooper/notebooks/reduction/EDisCS/Ha_catalogs/gaussian_prior/1138_LDP.txt', 'r')
+    lines = f.readlines()[1:]
+    f.close()
+    
+    #create arrays 
+    LDP_UV    = [] 
+    LDP_VJ   = []
+    
+    
+    #pull array column 
+    for line in lines: 
+        a = line.split()
+        LDP_UV.append(float(a[3]))
+        LDP_VJ.append(float(a[2]))
+        
+        
+    LDP_UV = np.array(LDP_UV)
+    LDP_VJ = np.array(LDP_VJ)
+
+
     plt.subplot(3,2,1)
-    plt.scatter(crf_VJ[np.where(np.logical_and(cz<0.4996,cz>0.4596))],crf_UV[np.where(np.logical_and(cz<0.4996,cz>0.4596))],c=cSFR[np.where(np.logical_and(cz<0.4996,cz>0.4596))])
+    plt.hist2d(LDP_VJ+0.105, LDP_UV+0.014, bins=(150, 150), cmap=plt.cm.Greys)
+    plt.scatter(crf_VJ[np.where(np.logical_and(cz<0.4996,cz>0.4596))]+0.105,crf_UV[np.where(np.logical_and(cz<0.4996,cz>0.4596))]+0.014,c=cSFR[np.where(np.logical_and(cz<0.4996,cz>0.4596))],s=100)
     cbar = plt.colorbar()
     cbar.set_label("log(SFR)")
     plt.xlabel('V-J')
@@ -260,17 +307,18 @@ def c1138(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
     
     plt.subplot(3,2,2)
+    plt.hist2d(LDP_VJ+0.105, LDP_UV+0.014, bins=(150, 150), cmap=plt.cm.Greys)
     #plt.scatter(rf_VJ[np.where(np.logical_and(z<0.4628,z>0.5028))],rf_UV[np.where(np.logical_and(z<0.4628,z>0.5028))],c=SFR[np.where(np.logical_and(z<0.4628,z>0.5028))])
-    plt.scatter(crf_VJ[np.where(cz<0.4596)],crf_UV[np.where(cz<0.4628)],c=cSFR[np.where(cz<0.4596)])
-    plt.scatter(crf_VJ[np.where(cz>0.4996)],crf_UV[np.where(cz>0.4996)],c=cSFR[np.where(cz>0.4996)])
-    plt.scatter(grf_VJ[np.where(gz<0.4596)],grf_UV[np.where(gz<0.4628)],c=gSFR[np.where(gz<0.4596)])
-    plt.scatter(grf_VJ[np.where(gz>0.4996)],grf_UV[np.where(gz>0.4996)],c=gSFR[np.where(gz>0.4996)])
+    plt.scatter(crf_VJ[np.where(cz<0.4596)]+0.105,crf_UV[np.where(cz<0.4596)]+0.014,c=cSFR[np.where(cz<0.4596)],s=100)
+    plt.scatter(crf_VJ[np.where(cz>0.4996)]+0.105,crf_UV[np.where(cz>0.4996)]+0.014,c=cSFR[np.where(cz>0.4996)],s=100)
+    plt.scatter(grf_VJ[np.where(gz<0.4596)]+0.105,grf_UV[np.where(gz<0.4596)]+0.014,c=gSFR[np.where(gz<0.4596)],s=100)
+    plt.scatter(grf_VJ[np.where(gz>0.4996)]+0.105,grf_UV[np.where(gz>0.4996)]+0.014,c=gSFR[np.where(gz>0.4996)],s=100)
     cbar = plt.colorbar()
-    cbar.set_clim(0,1)
+    cbar.set_clim(0.2,1)
     cbar.set_label("log(SFR)")
     plt.xlabel('V-J')
     plt.ylabel('U-V')
@@ -278,11 +326,12 @@ def c1138(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
     
     plt.subplot(3,2,3)
-    plt.scatter(grf_VJ[np.where(np.logical_and(gz<0.4996,gz>0.4596))],grf_UV[np.where(np.logical_and(gz<0.4996,gz>0.4596))],c=gSFR[np.where(np.logical_and(gz<0.4996,gz>0.4596))])
+    plt.hist2d(LDP_VJ+0.105, LDP_UV+0.014, bins=(150, 150), cmap=plt.cm.Greys)
+    plt.scatter(grf_VJ[np.where(np.logical_and(gz<0.4996,gz>0.4596))]+0.105,grf_UV[np.where(np.logical_and(gz<0.4996,gz>0.4596))]+0.014,c=gSFR[np.where(np.logical_and(gz<0.4996,gz>0.4596))],s=100)
     cbar = plt.colorbar()
     cbar.set_label("log(SFR)")
     plt.xlabel('V-J')
@@ -291,18 +340,18 @@ def c1138(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
     
 
     
     plt.subplot(3,2,4)
-    plt.scatter(crf_m[np.where((cz>0.4596)&(cz<0.4996))],cSFR[np.where((cz>0.4596)&(cz<0.4996))],color='blue',alpha=0.7,marker = 'o')
-    plt.scatter(grf_m[np.where((gz>0.4596)&(gz<0.4996))],gSFR[np.where((gz>0.4596)&(gz<0.4996))],color='blue',alpha=0.5,marker = 'x')
-    plt.scatter(crf_m[np.where(cz<0.4596)],cSFR[np.where(cz<0.4596)],c='red',alpha=0.5)
-    plt.scatter(crf_m[np.where(cz>0.4996)],cSFR[np.where(cz>0.4996)],c='red',alpha=0.5)
-    plt.scatter(grf_m[np.where(gz<0.4596)],gSFR[np.where(gz<0.4596)],c='red',alpha=0.5)
-    plt.scatter(grf_m[np.where(gz>0.4996)],gSFR[np.where(gz>0.4996)],c='red',alpha=0.5) 
+    plt.scatter(crf_m[np.where((cz>0.4596)&(cz<0.4996))],cSFR[np.where((cz>0.4596)&(cz<0.4996))],color='blue',alpha=0.7,marker = 'o',s=100)
+    plt.scatter(grf_m[np.where((gz>0.4596)&(gz<0.4996))],gSFR[np.where((gz>0.4596)&(gz<0.4996))],color='blue',alpha=0.5,marker = 'x',s=100)
+    plt.scatter(crf_m[np.where(cz<0.4596)],cSFR[np.where(cz<0.4596)],c='red',alpha=0.5,s=100)
+    plt.scatter(crf_m[np.where(cz>0.4996)],cSFR[np.where(cz>0.4996)],c='red',alpha=0.5,s=100)
+    plt.scatter(grf_m[np.where(gz<0.4596)],gSFR[np.where(gz<0.4596)],c='red',alpha=0.5,s=100)
+    plt.scatter(grf_m[np.where(gz>0.4996)],gSFR[np.where(gz>0.4996)],c='red',alpha=0.5,s=100) 
     plt.xlim(7.5,11)
     #plt.ylim(-1,3)
     plt.xlabel('logM')
@@ -426,7 +475,7 @@ def c1059(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
     
     plt.subplot(3,2,2)
@@ -445,7 +494,7 @@ def c1059(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
 
     plt.subplot(3,2,3)
@@ -459,7 +508,7 @@ def c1059(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
 
     plt.subplot(3,2,4)
@@ -556,8 +605,28 @@ def c1227(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     y2 = [1.3,2]
     y3 = [2,5]
 
+    f = open('/data2/jrcooper/notebooks/reduction/EDisCS/Ha_catalogs/gaussian_prior/1227_LDP.txt', 'r')
+    lines = f.readlines()[1:]
+    f.close()
+    
+    #create arrays 
+    LDP_UV    = [] 
+    LDP_VJ   = []
+    
+    
+    #pull array column 
+    for line in lines: 
+        a = line.split()
+        LDP_UV.append(float(a[3]))
+        LDP_VJ.append(float(a[2]))
+        
+        
+    LDP_UV = np.array(LDP_UV)
+    LDP_VJ = np.array(LDP_VJ)
+
     plt.subplot(3,2,1)
-    plt.scatter(crf_VJ[np.where(np.logical_and(cz<0.6557,cz>0.6157))],crf_UV[np.where(np.logical_and(cz<0.6557,cz>0.6157))],c=cSFR[np.where(np.logical_and(cz<0.6557,cz>0.6157))])
+    plt.hist2d(LDP_VJ-0.195, LDP_UV+0.114, bins=(150, 150), cmap=plt.cm.Greys)
+    plt.scatter(crf_VJ[np.where(np.logical_and(cz<0.6557,cz>0.6157))]-0.195,crf_UV[np.where(np.logical_and(cz<0.6557,cz>0.6157))]+0.114,c=cSFR[np.where(np.logical_and(cz<0.6557,cz>0.6157))],s=100)
     cbar = plt.colorbar()
     cbar.set_label("log(SFR)")
     plt.xlabel('V-J')
@@ -566,17 +635,18 @@ def c1227(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
     
     plt.subplot(3,2,2)
+    plt.hist2d(LDP_VJ-0.195, LDP_UV+0.114, bins=(150, 150), cmap=plt.cm.Greys)
     #plt.scatter(rf_VJ[np.where(np.logical_and(z<0.4628,z>0.5028))],rf_UV[np.where(np.logical_and(z<0.4628,z>0.5028))],c=SFR[np.where(np.logical_and(z<0.4628,z>0.5028))])
-    plt.scatter(crf_VJ[np.where(cz<0.6157)],crf_UV[np.where(cz<0.6157)],c=cSFR[np.where(cz<0.6157)])
-    plt.scatter(crf_VJ[np.where(cz>0.6557)],crf_UV[np.where(cz>0.6557)],c=cSFR[np.where(cz>0.6557)])
-    plt.scatter(grf_VJ[np.where(gz<0.6157)],grf_UV[np.where(gz<0.6157)],c=gSFR[np.where(gz<0.6157)])
-    plt.scatter(grf_VJ[np.where(gz>0.6557)],grf_UV[np.where(gz>0.6557)],c=gSFR[np.where(gz>0.6557)])
+    plt.scatter(crf_VJ[np.where(cz<0.6157)]-0.195,crf_UV[np.where(cz<0.6157)]+0.114,c=cSFR[np.where(cz<0.6157)],s=100)
+    plt.scatter(crf_VJ[np.where(cz>0.6557)]-0.195,crf_UV[np.where(cz>0.6557)]+0.114,c=cSFR[np.where(cz>0.6557)],s=100)
+    plt.scatter(grf_VJ[np.where(gz<0.6157)]-0.195,grf_UV[np.where(gz<0.6157)]+0.114,c=gSFR[np.where(gz<0.6157)],s=100)
+    plt.scatter(grf_VJ[np.where(gz>0.6557)]-0.195,grf_UV[np.where(gz>0.6557)]+0.114,c=gSFR[np.where(gz>0.6557)],s=100)
     cbar = plt.colorbar()
-    cbar.set_clim(0,0.5)
+    cbar.set_clim(0,1)
     cbar.set_label("log(SFR)")
     plt.xlabel('V-J')
     plt.ylabel('U-V')
@@ -584,11 +654,12 @@ def c1227(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
 
     plt.subplot(3,2,3)
-    plt.scatter(grf_VJ[np.where(np.logical_and(gz<0.6557,gz>0.6157))],grf_UV[np.where(np.logical_and(gz<0.6557,gz>0.6157))],c=gSFR[np.where(np.logical_and(gz<0.6557,gz>0.6157))])
+    plt.hist2d(LDP_VJ-0.195, LDP_UV+0.114, bins=(150, 150), cmap=plt.cm.Greys)
+    plt.scatter(grf_VJ[np.where(np.logical_and(gz<0.6557,gz>0.6157))]-0.195,grf_UV[np.where(np.logical_and(gz<0.6557,gz>0.6157))]+0.114,c=gSFR[np.where(np.logical_and(gz<0.6557,gz>0.6157))],s=100)
     cbar = plt.colorbar()
     cbar.set_label("log(SFR)")
     plt.xlabel('V-J')
@@ -597,16 +668,16 @@ def c1227(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.plot(x1,y1,color='black')
     plt.plot(x2,y2,color='black')
     plt.plot(x3,y3,color='black')
-    plt.xlim(-0.5,2)
+    plt.xlim(-0.5,3)
     plt.ylim(0,3)
 
     plt.subplot(3,2,4)
-    plt.scatter(crf_m[np.where((cz>0.6157)&(cz<0.6557))],cSFR[np.where((cz>0.6157)&(cz<0.6557))],color='blue',alpha=0.7,marker = 'o')
-    plt.scatter(grf_m[np.where((gz>0.6157)&(gz<0.6557))],gSFR[np.where((gz>0.6157)&(gz<0.6557))],color='blue',alpha=0.5,marker = 'x')
-    plt.scatter(crf_m[np.where(cz<0.6157)],cSFR[np.where(cz<0.6157)],c='red',alpha=0.5)
-    plt.scatter(crf_m[np.where(cz>0.6557)],cSFR[np.where(cz>0.6557)],c='red',alpha=0.5)
-    plt.scatter(grf_m[np.where(gz<0.6157)],gSFR[np.where(gz<0.6157)],c='red',alpha=0.5)
-    plt.scatter(grf_m[np.where(gz>0.6557)],gSFR[np.where(gz>0.6557)],c='red',alpha=0.5)   
+    plt.scatter(crf_m[np.where((cz>0.6157)&(cz<0.6557))],cSFR[np.where((cz>0.6157)&(cz<0.6557))],color='blue',alpha=0.7,marker = 'o',s=100)
+    plt.scatter(grf_m[np.where((gz>0.6157)&(gz<0.6557))],gSFR[np.where((gz>0.6157)&(gz<0.6557))],color='blue',alpha=0.5,marker = 'x',s=100)
+    plt.scatter(crf_m[np.where(cz<0.6157)],cSFR[np.where(cz<0.6157)],c='red',alpha=0.5,s=100)
+    plt.scatter(crf_m[np.where(cz>0.6557)],cSFR[np.where(cz>0.6557)],c='red',alpha=0.5,s=100)
+    plt.scatter(grf_m[np.where(gz<0.6157)],gSFR[np.where(gz<0.6157)],c='red',alpha=0.5,s=100)
+    plt.scatter(grf_m[np.where(gz>0.6557)],gSFR[np.where(gz>0.6557)],c='red',alpha=0.5,s=100)   
     plt.xlim(7.5,11)
     #plt.ylim(-1,3)
     plt.xlabel('logM')
@@ -619,4 +690,61 @@ def c1227(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
     plt.legend(handles=[red_patch,blue_patch,blue_star],loc=2)
 
     plt.show()
+
+def SFR_SN(cf_Ha,gf_Ha,crf_VJ,crf_UV,crf_m,cz):
+    plt.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
+    f = open('/data2/jrcooper/notebooks/reduction/EDisCS/Ha_catalogs/gaussian_prior/all_SN_SFR.txt', 'r')
+    lines = f.readlines()[1:]
+    f.close()
+    
+            #create arrays 
+    cf_Ha    = []
+    cf_err   = []
+    crf_VJ   = []
+    crf_UV   = []
+    crf_m    = []
+    cz       = []
+  
+    
+    #pull array column 
+    for line in lines: 
+        a = line.split()
+        cf_Ha.append(float(a[4]))
+        cf_err.append(float(a[5]))
+        crf_VJ.append(float(a[6]))
+        crf_UV.append(float(a[7]))
+        crf_m.append(float(a[8]))
+        cz.append(float(a[3]))
+        
+    cf_Ha = np.array(cf_Ha)
+    cf_err = np.array(cf_err)
+    cf_Ha_log = np.log10(cf_Ha)
+    crf_VJ = np.array(crf_VJ)
+    crf_UV = np.array(crf_UV)
+    crf_m  = np.array(crf_m)
+    cz     = np.array(cz)
+    cD_l_mpc = np.array(cosmo.luminosity_distance(cz))
+    cD_l_cm = cD_l_mpc*3.08567758128E+24
+    clogLHa = np.log10(4*np.pi*cf_Ha*cD_l_cm**2)
+    cSFR = clogLHa - 41.27
+    cSN = cf_Ha/cf_err
+
+    plt.scatter(crf_m,cSFR,c=cSN)
+    cbar = plt.colorbar()
+    cbar.set_label("SN")
+    cbar.set_clim(0,5)
+    plt.xlim(7.5,12)
+    #plt.ylim(-1,3)
+    plt.xlabel('logM')
+    plt.ylabel('logSFR')
+    #plt.title('All pointings matched with LPD Halpha>0')
+    #red_patch = mpatches.Patch(color='red', label='Field', alpha = 0.5)
+    #blue_patch = mpatches.Patch(color='blue', label='Core', alpha = 0.5)
+    #blue_star = mlines.Line2D([], [], color='blue', marker='x', linestyle='None',
+                         # markersize=10, label='Groups')
+    #plt.legend(handles=[red_patch,blue_patch,blue_star],loc=2)
+
+    plt.show()
+
+                      
     
